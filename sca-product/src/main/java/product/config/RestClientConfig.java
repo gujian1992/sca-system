64e880bf2347029016513ee53233cd87caadf2ec
liuyongkang <liuyongkang@situdata.com>
2022-02-15 16:45:08 +0800

    使用nacos作配置中心

--- sca-product/src/main/java/product/config/RestClientConfig.java
+++ sca-product/src/main/java/product/config/RestClientConfig.java
@@ -1,24 +1,24 @@
-package product.config;
-
-import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.RestClients;
-import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
-
-/**
- * @author: lyk
- * @date: 2022/2/12 10:14
- * @description:
- */
-public class RestClientConfig extends AbstractElasticsearchConfiguration {
-    @Override
-    @Bean
-    public RestHighLevelClient elasticsearchClient()  {
-        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
-                .build();
-        return RestClients.create(clientConfiguration).rest();
-    }
-
-}
+//package product.config;
+//
+//import org.elasticsearch.client.RestHighLevelClient;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.data.elasticsearch.client.ClientConfiguration;
+//import org.springframework.data.elasticsearch.client.RestClients;
+//import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+//
+///**
+// * @author: lyk
+// * @date: 2022/2/12 10:14
+// * @description:
+// */
+//public class RestClientConfig extends AbstractElasticsearchConfiguration {
+//    @Override
+//    @Bean
+//    public RestHighLevelClient elasticsearchClient()  {
+//        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+//                .connectedTo("localhost:9200")
+//                .build();
+//        return RestClients.create(clientConfiguration).rest();
+//    }
+//
+//}
